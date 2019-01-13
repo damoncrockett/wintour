d174dc777ba4801a9f8b8d8bf04a6d35a361404a
damoncrockett <damoncrockett@hotmail.com>
2019-01-13 04:26:31 -0500

    min working d3
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,39 +1,33 @@
 import { json } from 'd3-fetch'
-import { select, selectAll } from 'd3-selection'
-
-json("src/assets/data/json/data.json", function(data) {
-    testView(data);
-});
+import { select } from 'd3-selection'
 
 const rectSide = 10;
 const rectPad = 1;
+const histWidth = 1000 + rectPad * 2;
+const histHeight = 5000;
 
-const testView = function(data) {
-  const histWidth = 1000 + rectPad * 2;
-  const histHeight = 5000;
+json("src/assets/data/json/data.json").then( data => {
+  var svg = select("body")
+              .append("svg")
+              .attr("width", histWidth)
+              .attr("height", histHeight);
 
-  let svg = d3.select("body")
-                .append("svg")
-                .attr("width", histWidth)
-                .attr("height", histHeight);
+  svg.selectAll("rect")
+     .data(data)
+     .enter()
+     .append("rect")
+     .attr("width", rectSide)
+     .attr("height", rectSide)
+     .attr("stroke", "#646464")
+     .attr("fill", function(d) {
+          return 'hsl(0,0%,'+String(parseInt((1-d.score)*100))+'%)';
+        })
+     .attr("x", function(d) {
+          return d.x * rectSide + rectPad;
+        })
+     .attr("y", function(d) {
+          return histHeight - d.y * rectSide - rectSide - rectPad;
+        });
 
-  let glyphs = svg.selectAll("rect")
-                  .data(data)
-                  .enter()
-                  .append("rect")
-                  .attr("width", rectSide)
-                  .attr("height", rectSide)
-                  .attr("stroke", "#646464");
-
-  glyphs.attr("fill", function(data) {
-    	      return 'hsl(0,0%,'+String(parseInt((1-data.score)*100))+'%)';
-          })
-          .attr("x", function(data) {
-              return data.x * rectSide + rectPad;
-          })
-          .attr("y", function(data) {
-              return histHeight - data.y * rectSide - rectSide - rectPad;
-          })
-
-    window.scrollTo( 0, 5000 ); // to keep equator in same visual spot
-};
+  window.scrollTo( 0, 5000 ); // to keep equator in same visual spot
+});
