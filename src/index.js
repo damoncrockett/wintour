36858b7f40c3bfed987fb733536ebcd653b8cfaf
damoncrockett <damoncrockett@hotmail.com>
2019-01-14 03:00:56 -0500

    simplify d3
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ const rectSide = 10;
 const rectPad = 1;
 const histWidth = 1000 + rectPad * 2;
 const histHeight = 5000;
-const bgcolor = '#ffffff';
 
 json("src/assets/data/json/data.json").then( data => {
-  var svg = select('body')
+  const svg = select('body')
             .append('svg')
             .attr('width', histWidth)
             .attr('height', histHeight);
@@ -20,15 +19,11 @@ json("src/assets/data/json/data.json").then( data => {
      .attr('width', rectSide)
      .attr('height', rectSide)
      .attr('stroke', '#646464')
-     .attr('fill', function(d) {
-          return 'hsl(0,0%,'+String(parseInt((1-d.score)*100))+'%)';
-        })
-     .attr('x', function(d) {
-          return d.x * rectSide + rectPad;
-        })
-     .attr('y', function(d) {
-          return histHeight - d.y * rectSide - rectSide - rectPad;
-        });
+     .attr('fill', d => score2hsl(d.score))
+     .attr('x', d => d.x * rectSide + rectPad)
+     .attr('y', d => histHeight - d.y * rectSide - rectSide - rectPad);
 
   window.scrollTo( 0, 5000 ); // to keep equator in same visual spot
 });
+
+const score2hsl = score => 'hsl(0,0%,'+String(parseInt((1-score)*100))+'%)';
