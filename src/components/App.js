be6886dded1000aa7b7c38a1d5b22992bc6983ef
damoncrockett <damoncrockett@hotmail.com>
2019-01-22 18:25:42 -0500

    unconnected toggle
    Signed-off-by: damoncrockett <damoncrockett@hotmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Histogram from './Histogram';
+import RiskToggle from './RiskToggle';
 import jsonData from '../assets/data/json/data.json';
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
         return (
             <div>
                 <Histogram data={jsonData}/>
+                <RiskToggle />
             </div>
         );
     }
---
+++ src/components/RiskToggle.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class RiskToggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({ //functional setState
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'RISK ON' : 'RISK OFF'}
+      </button>
+    );
+  }
+}
+
+export default RiskToggle;
